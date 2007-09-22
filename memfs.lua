9757d264661628cc8fe2ede7530b0fadf35beb25
garyng2000 <garyng2000@ce3af2bc-4435-0410-bec2-e7f8933db683>
2007-09-22 01:24:08 +0000

    minor tweak for the node mode bits handling

--- memfs.lua
+++ memfs.lua
@@ -36,6 +36,50 @@ function string:splitpath()
     return dir:match("(.-)[/\\]?$"), file
 end
 
+local tab = {  -- tab[i+1][j+1] = xor(i, j) where i,j in (0-15)
+  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, },
+  {1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14, },
+  {2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13, },
+  {3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12, },
+  {4, 5, 6, 7, 0, 1, 2, 3, 12, 13, 14, 15, 8, 9, 10, 11, },
+  {5, 4, 7, 6, 1, 0, 3, 2, 13, 12, 15, 14, 9, 8, 11, 10, },
+  {6, 7, 4, 5, 2, 3, 0, 1, 14, 15, 12, 13, 10, 11, 8, 9, },
+  {7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, },
+  {8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, },
+  {9, 8, 11, 10, 13, 12, 15, 14, 1, 0, 3, 2, 5, 4, 7, 6, },
+  {10, 11, 8, 9, 14, 15, 12, 13, 2, 3, 0, 1, 6, 7, 4, 5, },
+  {11, 10, 9, 8, 15, 14, 13, 12, 3, 2, 1, 0, 7, 6, 5, 4, },
+  {12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3, },
+  {13, 12, 15, 14, 9, 8, 11, 10, 5, 4, 7, 6, 1, 0, 3, 2, },
+  {14, 15, 12, 13, 10, 11, 8, 9, 6, 7, 4, 5, 2, 3, 0, 1, },
+  {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, },
+}
+
+local function _bxor (a,b)
+    local res, c = 0, 1
+    while a > 0 and b > 0 do
+        local a2, b2 = a % 16, b % 16
+        res = res + tab[a2+1][b2+1]*c
+        a = (a-a2)/16
+        b = (b-b2)/16
+        c = c*16
+    end
+    res = res + a*c + b*c
+    return res
+end
+
+
+local ff = 2^32 - 1
+local function _bnot(a) return ff - a end
+
+local function _band(a,b) return ((a+b) - _bxor(a,b))/2 end
+
+local function _bor(a,b) return ff - _band(ff - a, ff - b) end
+
+local function set_bits(mode, bits)
+    return _bor(mode, bits)
+end
+
 local function is_dir(mode)
     local o = ((mode - mode % S_IFDIR)/S_IFDIR) % 2
     return o ~= 0
@@ -219,7 +263,7 @@ mkdir = function(self, path, mode, ...)
     local dirent,parent = dir_walk(root, path)
     local uid,gid,pid = fuse.context()
     local x = {
-        mode = mode+S_IFDIR, -- mode don't have directory bit set
+        mode = set_bits(mode,S_IFDIR), -- mode don't have directory bit set
         ino = 0, 
         dev = 0, 
         nlink = 2, uid = uid, gid = gid, size = 0, atime = now(), mtime = now(), ctime = now()}
@@ -237,7 +281,7 @@ create = function(self, path, mode, flag, ...)
     local dirent,parent = dir_walk(root, path)
     local uid,gid,pid = fuse.context()
     local x = {
-        mode = mode,
+        mode = set_bits(mode, S_IFREG),
         ino = 0, 
         dev = 0, 
         nlink = 1, uid = uid, gid = gid, size = 0, atime = now(), mtime = now(), ctime = now()}
