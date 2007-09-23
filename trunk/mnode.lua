1b6c24804c0122e1947966d9bb5330cfac0b73bb
garyng2000 <garyng2000@ce3af2bc-4435-0410-bec2-e7f8933db683>
2007-09-23 06:47:46 +0000

    forgot to add the abstracted out memory node module

---
+++ trunk/mnode.lua
@@ -0,0 +1,66 @@
+module('mnode', package.seeall)
+
+local nodes = {}
+local blocks = setmetatable({},{__mode="v"})
+
+function get(key)
+    return nodes[key]
+end
+
+function set(key, v)
+    nodes[key] = v
+end
+
+function get_block(key)
+    return blocks[key]
+end
+
+function flush_node(node, path, final)
+    --print('flush_node', path, final)
+    return true
+end
+
+function flush_data(block, path, final)
+    if path:find('git') then print('flush_data', path, final) end
+    return true
+end
+
+local block_mt = {
+    __index = function(o,k) 
+        return rawget(o,'_data')[k]
+    end,
+    __newindex = function(o, k, v)
+        local x = rawget(o,'_data')
+        x[k] = v
+    end,
+    __call = function(o,t,i)
+        return next(rawget(o, '_data'), i) 
+    end
+    }
+
+local node_mt = {
+    __index = function(o,k) 
+        return rawget(o,'_data')[k]
+    end,
+    __newindex = function(o, k, v)
+        local x = rawget(o,'_data')
+        x[k] = v
+    end,
+    __call = function(o,t,i)
+        return next(rawget(o, '_data'), i) 
+    end
+    }
+
+function block(t)
+    local key = function() end
+    local n =  setmetatable({_key= key, _data=t or {}}, block_mt)
+    blocks[key] = n
+    return n
+end
+
+function node(t)
+    local key = function() end
+    local n =  setmetatable({_key= key, _data=t or {}}, node_mt)
+    nodes[key] = n
+    return n
+end
